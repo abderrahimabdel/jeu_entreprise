222a2505ef00aec6dacbb880eafb7ed9f4a7ecc3
abderrahimabdel <113397059+abderrahimabdel@users.noreply.github.com>
2022-10-07 13:27:10 +0200

    Update settings.py

--- jeu_entreprise/settings.py
+++ jeu_entreprise/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
@@ -131,7 +132,6 @@ STATICFILES_DIRS = [
     BASE_DIR / "static/js",
     BASE_DIR / "static/css"
 ]
-DISABLE_COLLECTSTATIC=1
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
