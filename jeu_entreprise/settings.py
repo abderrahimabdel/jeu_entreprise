2479d4c0ea2dc70df459af594b9b147119f93d8c
abderrahimabdel <113397059+abderrahimabdel@users.noreply.github.com>
2023-01-07 23:32:45 +0100

    Update settings.py

--- jeu_entreprise/settings.py
+++ jeu_entreprise/settings.py
@@ -131,7 +131,8 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static/js",
-    BASE_DIR / "static/css"
+    BASE_DIR / "static/css",
+    BASE_DIR / "static/images"
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
